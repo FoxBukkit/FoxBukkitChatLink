b4bbe4ef30ad88f2f65d03b83196a470af109ba4
Doridian Draconia <mriq91@gmail.com>
2014-02-03 11:32:25 +0100

    Added deployscript handler

--- src/main/java/de/doridian/yiffbukkit/chatlink/Main.java
+++ src/main/java/de/doridian/yiffbukkit/chatlink/Main.java
@@ -1,5 +1,7 @@
 package de.doridian.yiffbukkit.chatlink;
 
+import java.io.File;
+
 public class Main {
 	public static void main(String[] args) {
 		new Thread() {
@@ -10,6 +12,23 @@ public class Main {
 		new Thread() {
 			public void run() {
 				new XmlRedisHandler();
+			}
+		}.start();
+
+		new Thread() {
+			public void run() {
+				while(true) {
+					try {
+						if(new File("YiffBukkitChatLink.jar.deploy").exists()) {
+							RedisManager.readJedisPool.destroy();
+							System.exit(0);
+							return;
+						}
+						Thread.sleep(5000);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
 			}
 		}.start();
 	}
