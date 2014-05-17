6899d5335e2e2affe6ab87cb99e814ddd8af03e1
Doridian Draconia <mriq91@gmail.com>
2014-05-17 18:48:31 +0200

    Add config file creation

--- src/main/java/de/doridian/yiffbukkit/chatlink/Main.java
+++ src/main/java/de/doridian/yiffbukkit/chatlink/Main.java
@@ -33,4 +33,8 @@ public class Main {
 			}
 		}
 	}
+
+    public static File getDataFolder() {
+        return new File(".");
+    }
 }
--- src/main/java/de/doridian/yiffbukkit/chatlink/config/ConfigFileReader.java
+++ src/main/java/de/doridian/yiffbukkit/chatlink/config/ConfigFileReader.java
@@ -1,10 +1,13 @@
 package de.doridian.yiffbukkit.chatlink.config;
 
+import de.doridian.yiffbukkit.chatlink.Main;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class ConfigFileReader extends FileReader {
 	public ConfigFileReader(String file) throws FileNotFoundException {
-		super("./" + file);
+		super(new File(Main.getDataFolder(), file));
 	}
 }
