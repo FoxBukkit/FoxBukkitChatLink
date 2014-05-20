07caa8975630cadd3cc1aaa4e40dea681588ee40
Doridian Draconia <mriq91@gmail.com>
2014-05-20 20:53:14 +0200

    Change list colors

--- src/main/java/de/doridian/yiffbukkit/chatlink/commands/ListCommand.java
+++ src/main/java/de/doridian/yiffbukkit/chatlink/commands/ListCommand.java
@@ -6,10 +6,9 @@ import de.doridian.yiffbukkit.chatlink.json.ChatMessage;
 import de.doridian.yiffbukkit.chatlink.json.MessageContents;
 import de.doridian.yiffbukkit.chatlink.util.CommandException;
 import de.doridian.yiffbukkit.chatlink.util.PlayerHelper;
-import de.doridian.yiffbukkit.chatlink.util.Utils;
 
 public class ListCommand extends ICommand {
-    private static final String LIST_FORMAT = "<color name=\"blue\">%1$s</color><color name=\"white\">: %2$s</color>";
+    private static final String LIST_FORMAT = "<color name=\"white\">%1$s: %2$s</color>";
 
     @Override
     public String[] getNames() {
@@ -22,12 +21,12 @@ public class ListCommand extends ICommand {
         for(String server : PlayerHelper.getAllServers()) {
             StringBuilder listTextB = new StringBuilder();
             for(Player ply : PlayerHelper.getOnlinePlayersOnServer(server)) {
-                listTextB.append(", ");
+                listTextB.append("\u00a7f, ");
                 listTextB.append(PlayerHelper.getPlayerRankTagRaw(ply.uuid));
                 listTextB.append(ply.name);
             }
             String listText = listTextB.substring(2);
-            message.contents = new MessageContents("\u00a79" + server + "\u00a7f: " + listText,
+            message.contents = new MessageContents("\u00a7f" + server + ": " + listText,
                     LIST_FORMAT,
                     new String[]{
                             server, listText
