65d8667afc88c2a7e11ce895904bd78e502e4254
Doridian Draconia <mriq91@gmail.com>
2014-05-20 20:57:20 +0200

    Change /list format

--- src/main/java/de/doridian/yiffbukkit/chatlink/commands/ListCommand.java
+++ src/main/java/de/doridian/yiffbukkit/chatlink/commands/ListCommand.java
@@ -8,7 +8,7 @@ import de.doridian.yiffbukkit.chatlink.util.CommandException;
 import de.doridian.yiffbukkit.chatlink.util.PlayerHelper;
 
 public class ListCommand extends ICommand {
-    private static final String LIST_FORMAT = "<color name=\"white\">%1$s:</color> %2$s";
+    private static final String LIST_FORMAT = "<color name=\"dark_gray\">[%1$s]</color> %2$s";
 
     @Override
     public String[] getNames() {
@@ -26,7 +26,7 @@ public class ListCommand extends ICommand {
                 listTextB.append(ply.name);
             }
             String listText = "\u00a7f" + listTextB.substring(4);
-            message.contents = new MessageContents("\u00a7f" + server + ": " + listText,
+            message.contents = new MessageContents("\u00a78[" + server + "] " + listText,
                     LIST_FORMAT,
                     new String[]{
                             server, listText
