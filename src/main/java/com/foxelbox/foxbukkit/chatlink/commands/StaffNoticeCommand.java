8006d5bec01a737e06b1e90e2261b25ed2411353
Adam Walker <me@adam-walker.me.uk>
2014-11-03 21:12:17 +0000

    Added /staffnotice

---
+++ src/main/java/com/foxelbox/foxbukkit/chatlink/commands/StaffNoticeCommand.java
@@ -0,0 +1,44 @@
+/**
+ * This file is part of FoxBukkitChatLink.
+ *
+ * FoxBukkitChatLink is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FoxBukkitChatLink is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with FoxBukkitChatLink.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.foxelbox.foxbukkit.chatlink.commands;
+
+import com.foxelbox.foxbukkit.chatlink.RedisHandler;
+import com.foxelbox.foxbukkit.chatlink.commands.system.ICommand;
+import com.foxelbox.foxbukkit.chatlink.json.ChatMessageIn;
+import com.foxelbox.foxbukkit.chatlink.json.ChatMessageOut;
+import com.foxelbox.foxbukkit.chatlink.json.MessageContents;
+import com.foxelbox.foxbukkit.chatlink.util.CommandException;
+
+@ICommand.Names({"staffnotice"})
+@ICommand.Help("Sends a highlighted staff message to all users")
+@ICommand.Usage("<text>")
+@ICommand.Permission("foxbukkit.staffnotice")
+public class StaffNoticeCommand extends ICommand {
+    private static final String OPCHAT_FORMAT = "<color name=\"red\">[#!STAFF]</color> " + RedisHandler.MESSAGE_FORMAT;
+
+    @Override
+    public ChatMessageOut run(ChatMessageIn messageIn, String formattedName, String argStr) throws CommandException {
+        final ChatMessageOut message = new ChatMessageOut(messageIn);
+        message.contents = new MessageContents("\u00a74[#!STAFF] \u00a7f" + formattedName + "\u00a7f: " + argStr,
+                OPCHAT_FORMAT,
+                new String[] {
+                        "SERVER", formattedName, argStr
+                });
+        message.importance = 3;
+        return message;
+    }
+}
