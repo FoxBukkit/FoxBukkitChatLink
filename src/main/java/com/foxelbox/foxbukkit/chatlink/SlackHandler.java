190adc7a29acacb231a53f1f8ecfd0097116965c
Michael Johnson <michael@johnson.computer>
2015-07-14 12:47:29 -0400

    Add debug print

--- src/main/java/com/foxelbox/foxbukkit/chatlink/SlackHandler.java
+++ src/main/java/com/foxelbox/foxbukkit/chatlink/SlackHandler.java
@@ -81,6 +81,8 @@ public class SlackHandler implements SlackMessagePostedListener {
 				return;
 			}
 
+			System.out.println("Received message from " + event.getSender().getUserName() + " in " + event.getChannel().getName());
+
 			final String channelName = event.getChannel().getName();
 			if(!channelName.equalsIgnoreCase("#minecraft") && !channelName.equalsIgnoreCase("#minecraft-ops"))
 				return;
